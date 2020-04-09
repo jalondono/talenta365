ad6f599a57481590e0541ea44b11227e9d476b18
jalondono <juanlondono151776@hotmail.com>
2020-04-09 08:31:30 -0500

    Update: - models.py: Added models to regions and cities - settings.py Added app and libraries

--- regions_cities/models.py
+++ regions_cities/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+status = [
+    ('1', 'ACTIVE'),
+    ('2', 'INACTIVE'),
+]
+
+
+class City(models.Model):
+    name = models.CharField(max_length=40, blank=False)
+    status = models.CharField(max_length=45, choices=status, default='ACTIVE')
+
+    def __str__(self):
+        return self.name
+
+
+class Regions(models.Model):
+    name = models.CharField(max_length=40, blank=False)
+    cities = models.ManyToManyField(City)
+
+    def __str__(self):
+        return self.name
